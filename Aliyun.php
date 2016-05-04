445aa16098adb6944fca185a0cce993feae8b32f
Enoch <15989529532@163.com>
2016-05-04 18:41:02 +0800

    增加删除object、批量删除object

--- Aliyun.php
+++ Aliyun.php
@@ -51,4 +51,33 @@ class Aliyun extends Component
         $bucketName = $bucketName ? $bucketName : $this->defaultBucketName;
         return $ossClient->uploadFile($bucketName, $objectName, $localFilePath);
     }
+
+    /**
+     * 删除Object
+     * @param $object
+     * @param null $options
+     * @param null $bucketName
+     * @return null
+     */
+    public function deleteObject($object, $options = NULL, $bucketName = null)
+    {
+        $ossClient = new OssClient($this->accessKeyId, $this->accessSecret, $this->endPoint);
+        $bucketName = $bucketName ? $bucketName : $this->defaultBucketName;
+        return $ossClient->deleteObject($bucketName, $object, $options);
+    }
+
+    /**
+     * 删除同一个Bucket中的多个Object
+     *
+     * @param $bucketName
+     * @param array $objects object列表
+     * @param array $options
+     * @return ResponseCore
+     */
+    public function deleteObjects($objects, $options = null, $bucketName = null)
+    {
+        $ossClient = new OssClient($this->accessKeyId, $this->accessSecret, $this->endPoint);
+        $bucketName = $bucketName ? $bucketName : $this->defaultBucketName;
+        return $ossClient->deleteObjects($bucketName, $objects, $options);
+    }
 }
